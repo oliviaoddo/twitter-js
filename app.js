edbaf9505b01843bb239e33203ad8268be5e5008
Alexander Zoitos <azoitos1@gmail.com>
2017-06-13 10:47:44 -0400

    Updated up to Routing

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const app = express();
 
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});
+
+
+var locals = {
+	title: 'People',
+	people: [
+		{name: 'Olivia'},
+		{name: 'Alex'},
+		{name: 'Karen'}
+		]
+};
+
 app.listen(3000, function(){
-    console.log("server listening");
+    console.log('server listening');
 });
+
 
 app.use((req, res, next) => {
     console.log(req.method, req.path, res.statusCode);
     next();
 });
 
+app.get('/', (req, res) => {
+    res.render('index', locals);
+    // res.end();
+});
 
 app.get('/news', (req, res) => {
     res.send('<h1>News</h1');
-    res.end();
+    // res.end();
 });
 
 
-app.get('/', (req, res) => {
-    res.send('<h1>Welcome</h1');
-    res.end();
+nunjucks.render('index.html', locals, function(err, res){
+	if (err){
+		throw err;
+	}
+	console.log(res);
 });
 
